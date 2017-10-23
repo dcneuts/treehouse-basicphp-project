3ee5132bdab232cdbc6d5b07ca7dbda8d1ba668c
Derek Neuts <dcneuts@gmail.com>
2017-10-22 20:14:40 -0700

    Created basic handling for form submission data using POST method, working on disallowing duplicates and error messages due to handling in the browser.

--- process.php
+++ process.php
@@ -2,3 +2,24 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $details = $_POST["details"];
+
+echo "<pre>";
+$email_body = "";
+$email_body .= "Name: " . $name . "\n";
+$email_body .= "Email: " . $email . "\n";
+$email_body .= "Details: " . $details . "\n";
+echo $email_body;
+echo "</pre>";
+
+// Todo: Send email
+
+$pageTitle = "Thank you";
+$section = Null;
+
+include ("inc/header.php");
+?>
+
+<div class="section page">
+    <h1>Thank You</h1>
+    <p>Thanks for the email! I&rsquo;ll check out your suggestion shortly!</p>
+</div>
---
+++ thanks.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Derek
+ * Date: 10/22/2017
+ * Time: 8:13 PM
+ */
