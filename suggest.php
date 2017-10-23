5684b32fda35a94280875f363d96fca88a7582d7
Derek Neuts <dcneuts@gmail.com>
2017-10-22 19:11:26 -0700

    Created basic user form for suggestions

--- suggest.php
+++ suggest.php
@@ -5,7 +5,27 @@ $section = "suggest";
 include("inc/header.php"); ?>
 
 <div class="section page">
-    <h1>Suggest a Media Item</h1>
+    <div class="wrapper">
+        <h1>Suggest a Media Item</h1>
+        <p>If you think there's something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
+    </div>
+    <form method="post">
+        <table>
+            <tr>
+                <th><label for="name">Name</label></th>
+                <td><input type="text" id="name" name="name"/></td>
+            </tr>
+            <tr>
+                <th><label for="email">Email</label></th>
+                <td><input type="text" id="email" name="email"/></td>
+            </tr>
+            <tr>
+                <th><label for="details">Suggest Item Details</label></th>
+                <td><textarea name="details" id="details"></textarea></td>
+            </tr>
+        </table>
+        <input type="submit" value="Send" />
+    </form>
 </div>
 
 <?php include("inc/footer.php"); ?>
