31692451404eebec22e0dc56ed9e2260ca7b9d5a
Derek Neuts <dcneuts@gmail.com>
2017-10-23 00:47:57 -0700

    Included form validation as well as a basic honeypot for bots.

--- suggest.php
+++ suggest.php
@@ -2,10 +2,20 @@
 // Execute only when form values have been submitted
 // uses REQUEST METHOD to check POST status
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = trim(filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING));
+    $email = trim(filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL));
+    $details = trim(filter_input(INPUT_POST,"details",FILTER_SANITIZE_SPECIAL_CHARS));
 
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $details = $_POST["details"];
+// validation
+    if ($name =="" || $email == "" || $details == "") {
+        echo "Please fill in the required fields: Name, Email and Details";
+        exit;
+    }
+// honeypot validation
+    if ($_POST["address"] !="") {
+        echo "Bad form input";
+        exit;
+    }
 
     echo "<pre>";
     $email_body = "";
@@ -29,7 +39,8 @@ include("inc/header.php"); ?>
         <h1>Suggest a Media Item</h1>
         <?php if (isset($_GET["status"]) && $_GET["status"] == "thanks") {
             echo "<p>Thanks for the email! I&rsquo;ll check out your suggestion shortly!</p>";
-}?>
+} else { ?>
+
         <p>If you think there's something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
     </div>
     <form method="post" action="suggest.php">
@@ -46,9 +57,16 @@ include("inc/header.php"); ?>
                 <th><label for="details">Suggest Item Details</label></th>
                 <td><textarea name="details" id="details"></textarea></td>
             </tr>
+            <tr style="display:none"><!--Honeypot for Bots-->
+                <th><label for="address">Address</label></th>
+                <td><input type="text" id="address" name="address" />
+                <p>Please leave this field blank.</p></td>
+            </tr>
         </table>
         <input type="submit" value="Send" />
     </form>
+    <?php } ?>
+
 </div>
 
 <?php include("inc/footer.php"); ?>
